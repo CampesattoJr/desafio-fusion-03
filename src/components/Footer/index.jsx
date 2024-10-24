da63a02db3e1919a36f6af46081b16f71b239c47
CampesattoJr <juniorcampesatto@gmail.com>
2024-10-24 18:43:18 -0400

    feat: footer criado e adicionado ao projeto

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+export const Footer = () => {
+    return (
+        <footer>
+            <p>© Frontend Fusion</p>
+            <p>
+                Desenvolvido por
+                <link rel="stylesheet" href="https://github.com/CampesattoJr">Sandro</link>
+            </p>
+        </footer>
+    )
+}
